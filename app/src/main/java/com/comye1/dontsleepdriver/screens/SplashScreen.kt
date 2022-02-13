fa0022498d4062cf38427ae0a2d3642a6df3d9bd
comye1 <comye1@seoultech.ac.kr>
2022-02-13 21:34:41 +0900

    #1 Splash Screen implemented

---
+++ app/src/main/java/com/comye1/dontsleepdriver/screens/SplashScreen.kt
@@ -0,0 +1,39 @@
+package com.comye1.dontsleepdriver.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.comye1.dontsleepdriver.R
+import kotlinx.coroutines.delay
+
+
+@Composable
+fun SplashScreen(toNext: () -> Unit) {
+    LaunchedEffect(key1 = true) {
+        delay(3000L)
+        toNext()
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Don't Sleep", style = MaterialTheme.typography.h3, fontWeight = FontWeight.Bold)
+        Text(text = "Driver!", style = MaterialTheme.typography.h3, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(50.dp))
+        Image(
+            painter = painterResource(id = R.drawable.driving_img),
+            contentDescription = "driving image",
+            modifier = Modifier.size(200.dp)
+        )
+    }
+}
