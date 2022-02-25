abe94c9eb6c140b817322f8683bb890b36c4ca5b
comye1 <comye1@seoultech.ac.kr>
2022-02-25 12:27:00 +0900

    #8 위치정보 수집하기 setup (manifest, gradle, constants, permission utility)

---
+++ app/src/main/java/com/comye1/dontsleepdriver/other/Constants.kt
@@ -0,0 +1,19 @@
+package com.comye1.dontsleepdriver.other
+
+object Constants {
+
+    const val ACTION_START_OR_RESUME_SERVICE = "ACTION_START_OR_RESUME_SERVICE"
+    const val ACTION_PAUSE_SERVICE = "ACTION_PAUSE_SERVICE"
+    const val ACTION_STOP_SERVICE = "ACTION_STOP_SERVICE"
+
+    const val ACTION_SHOW_DSD_ACTIVITY = "ACTION_SHOW_DSD_ACTIVITY"
+
+    const val LOCATION_UPDATE_INTERVAL = 5000L
+    const val FASTEST_LOCATION_INTERVAL = 2000L
+
+    const val NOTIFICATION_CHANNEL_ID = "tracking_channel"
+    const val NOTIFICATION_CHANNEL_NAME = "tracking"
+    const val NOTIFICATION_ID = 1
+
+
+}
