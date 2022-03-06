2165ec65f804f403eeeb7efa19e235489b879d1d
comye1 <comye1@seoultech.ac.kr>
2022-03-07 00:27:26 +0900

    #14 HistoryViewModel

--- app/src/main/java/com/comye1/dontsleepdriver/history/HistoryViewModel.kt
+++ app/src/main/java/com/comye1/dontsleepdriver/history/HistoryViewModel.kt
@@ -2,8 +2,8 @@ package com.comye1.dontsleepdriver.history
 
 import androidx.lifecycle.ViewModel
 import com.comye1.dontsleepdriver.data.model.Driving
-import com.comye1.dontsleepdriver.data.model.LatLngSlp
 import com.comye1.dontsleepdriver.repository.DSDRepository
+import com.google.android.gms.maps.model.LatLng
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -15,19 +15,23 @@ class HistoryViewModel @Inject constructor(
 
     val drivingList = listOf(
         Driving(
-            startTime = "2022-02-27 20:49:19",
-            endTime = "2022-02-27 20:58:43",
+            startTime = 1646541190226,
+            endTime = 1646541190226,
             gpsData = listOf(
-                LatLngSlp(37.625500, 127.079877, 0),
-                LatLngSlp(37.627699, 127.081415, 4),
-                LatLngSlp(37.627962, 127.084926, 1),
-                LatLngSlp(37.626305, 127.085760, 2),
-                LatLngSlp(37.622602, 127.086884, 0),
-                LatLngSlp(37.620631, 127.087520, 0),
-                LatLngSlp(37.618024, 127.091351, 1),
-                LatLngSlp(37.616212, 127.100279, 2),
-                LatLngSlp(37.614720, 127.105728, 0)
-            )
+                LatLng(37.625500, 127.079877),
+                LatLng(37.627699, 127.081415),
+                LatLng(37.627962, 127.084926),
+                LatLng(37.626305, 127.085760),
+                LatLng(37.626305, 127.085760),
+                LatLng(37.626305, 127.085760),
+                LatLng(37.626305, 127.085760),
+                LatLng(37.622602, 127.086884),
+                LatLng(37.620631, 127.087520),
+                LatLng(37.618024, 127.091351),
+                LatLng(37.616212, 127.100279),
+                LatLng(37.614720, 127.105728)
+            ),
+            sleepData = listOf(0, 4, 1, 2, -1, -1, -1, 0, 0, 1, 2, 0)
         )
     )
 }
