056b5b23e0c4f018cdbcefb91a2e0b62612e980d
comye1 <comye1@seoultech.ac.kr>
2022-03-09 12:49:10 +0900

    #15 Naver oauth SDK initialization

--- app/src/main/java/com/comye1/dontsleepdriver/DSDApplication.kt
+++ app/src/main/java/com/comye1/dontsleepdriver/DSDApplication.kt
@@ -2,6 +2,7 @@ package com.comye1.dontsleepdriver
 
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
+import com.navercorp.nid.NaverIdLoginSDK
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -10,5 +11,6 @@ class DSDApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         KakaoSdk.init(this, BuildConfig.KAKAO_API_KEY)
+        NaverIdLoginSDK.initialize(this, "MnQQeQ2vJT_R8VE9zQCY", BuildConfig.NAVER_API_KEY, "Don't Sleep Driver")
     }
 }
