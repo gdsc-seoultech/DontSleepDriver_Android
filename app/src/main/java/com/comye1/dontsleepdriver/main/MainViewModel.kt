7e6b08bea81e9b914db5d421c0f3a310347507f5
comye1 <comye1@seoultech.ac.kr>
2022-03-25 21:18:51 +0900

    #10 총 운전 시간 tracking

--- app/src/main/java/com/comye1/dontsleepdriver/main/MainViewModel.kt
+++ app/src/main/java/com/comye1/dontsleepdriver/main/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.comye1.dontsleepdriver.main
 
+import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -7,6 +8,7 @@ import com.comye1.dontsleepdriver.R
 import com.comye1.dontsleepdriver.data.model.LocalUser
 import com.comye1.dontsleepdriver.repository.DSDRepository
 import com.comye1.dontsleepdriver.util.Resource
+import com.google.android.gms.maps.model.LatLng
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -21,15 +23,48 @@ class MainViewModel @Inject constructor(
     private val _user: MutableStateFlow<LocalUser?> = MutableStateFlow(null)
     val user: StateFlow<LocalUser?> = _user
 
+    var curTimeText = mutableStateOf("00:00:00")
+        private set
+
     val selectedSound = mutableStateOf(-1)
 
+    val isTracking = mutableStateOf(false)
+
+    val isSaved = mutableStateOf(false)
+
+    val gpsList = mutableListOf<LatLng>()
+
+    val sleepList = mutableListOf<Int>()
+
+    fun updateList(gps: LatLng, level: Int) {
+        gpsList.add(gps)
+        sleepList.add(level)
+    }
+
+    fun saveDriving() {
+        // 0인 gps 제거
+        Log.d("driving", gpsList.joinToString(" "))
+        Log.d("driving", sleepList.joinToString(""))
+        isSaved.value = true
+    }
+
+
+
+    fun setTrackingState(state: Boolean){
+        isTracking.value = state
+    }
+
+    fun updateTimeText(newTime: String) {
+        curTimeText.value = newTime
+    }
+
     private fun getSelectedSound() {
-        if (user.value != null){
+        if (user.value != null) {
             repository.getSavedSound(user.value!!.email).also {
-                selectedSound.value = if (it != -1) it else R.raw.rooster
+                selectedSound.value = if (it != -1) it else R.raw.sound_alarm_tone
             }
-        }else {
-            selectedSound.value = R.raw.rooster
+        } else {
+            selectedSound.value = R.raw.sound_alarm_tone
         }
         // R.raw.rooster
     }
