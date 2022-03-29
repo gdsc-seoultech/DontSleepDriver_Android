8cbc1c18f93dbe02ea63c24de7037496faf84f34
comye1 <comye1@seoultech.ac.kr>
2022-03-29 22:31:01 +0900

    googleSignInClient

--- app/src/main/java/com/comye1/dontsleepdriver/util/GoogleSignInClient.kt
+++ app/src/main/java/com/comye1/dontsleepdriver/util/GoogleSignInClient.kt
@@ -5,13 +5,9 @@ import com.comye1.dontsleepdriver.R
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.Scopes
-import com.google.android.gms.common.api.Scope
 
 fun getGoogleSignInClient(context: Context): GoogleSignInClient {
     val signInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-        .requestScopes(Scope(Scopes.PLUS_LOGIN))
-        .requestScopes(Scope(Scopes.PLUS_ME))
         .requestEmail()
         .requestIdToken(context.getString(R.string.web_client_id))
         .build()
